65ab2ebcbd48215aa2b1b221ba8f6a2954a13f5c
JAFFA onefour <tenflix14@gmail.com>
2023-08-10 17:00:08 +0530

    Updates

---
+++ precache-manifest.5cd7ac97e1886402a9c2b70fda8fb1cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9da782cfa099cb832b3",
+    "url": "/dinemate/static/css/main.f06f92fe.chunk.css"
+  },
+  {
+    "revision": "a9da782cfa099cb832b3",
+    "url": "/dinemate/static/js/main.a9da782c.chunk.js"
+  },
+  {
+    "revision": "f94ddaad2922c41190f1",
+    "url": "/dinemate/static/js/1.f94ddaad.chunk.js"
+  },
+  {
+    "revision": "db3bae8ba681887381e1",
+    "url": "/dinemate/static/css/2.0a088e20.chunk.css"
+  },
+  {
+    "revision": "db3bae8ba681887381e1",
+    "url": "/dinemate/static/js/2.db3bae8b.chunk.js"
+  },
+  {
+    "revision": "ad41bcba6d254132129a",
+    "url": "/dinemate/static/js/runtime~main.ad41bcba.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/dinemate/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "23f3823a8760c419105a10bb73bd0b15",
+    "url": "/dinemate/index.html"
+  }
+];
